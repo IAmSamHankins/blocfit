ce2f285b6e3055f1bb201c36891e9d5c366f8077
Sam Hankins <samantha.hankins@icloud.com>
2015-12-27 11:21:02 -0600

    chart chart chart

--- js/app.js
+++ js/app.js
@@ -2,29 +2,87 @@
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
 
-var weeklyRunTotals = {
-      labels : ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"],
-      datasets : [
-        {
-          fillColor : "rgba(220,220,220,0.5)",
-          strokeColor : "rgba(220,220,220,1)",
-          pointColor : "rgba(220,220,220,1)",
-          pointStrokeColor : "#fff",
-          data : [5,5,5,6,6,5,5]
-        },
-        {
-          fillColor : "rgba(52,152,219,0.5)",
-          strokeColor : "rgba(52,152,219,1)",
-          pointColor : "rgba(52,152,219,1)",
-          pointStrokeColor : "#fff",
-          data : [2.8,4.8,4.0,1.9,9.6,2.7,10]
-        }
-      ]
+var ctx = document.getElementById("myChart").getContext("2d");
+var myNewChart = new Chart(ctx).PolarArea(data);
+
+var data = {
+  labels: ["January", "February", "March", "April", "May", "June", "July"],
+  datasets: [
+    {
+      label: "My First dataset",
+
+      // Boolean - if true fill the area under the line
+      fill: false,
+
+      // String - the color to fill the area under the line with if fill is true
+      backgroundColor: "rgba(220,220,220,0.2)",
+
+      // The properties below allow an array to be specified to change the value of the item at the given index
+
+      // String or array - Line color
+      borderColor: "rgba(220,220,220,1)",
+
+      // String - cap style of the line. See https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/lineCap
+            borderCapStyle: 'butt',
+
+            // Array - Length and spacing of dashes. See https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/setLineDash
+            borderDash: [],
+
+            // Number - Offset for line dashes. See https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/lineDashOffset
+            borderDashOffset: 0.0,
+
+            // String - line join style. See https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/lineJoin
+            borderJoinStyle: 'miter',
+
+      // String or array - Point stroke color
+      pointBorderColor: "rgba(220,220,220,1)",
+
+      // String or array - Point fill color
+      pointBackgroundColor: "#fff",
+
+      // Number or array - Stroke width of point border
+      pointBorderWidth: 1,
+
+      // Number or array - Radius of point when hovered
+      pointHoverRadius: 5,
+
+      // String or array - point background color when hovered
+      pointHoverBackgroundColor: "rgba(220,220,220,1)",
+
+      // Point border color when hovered
+      pointHoverBorderColor: "rgba(220,220,220,1)",
+
+      // Number or array - border width of point when hovered
+      pointHoverBorderWidth: 2,
+
+      // Tension - bezier curve tension of the line. Set to 0 to draw straight Wlines connecting points
+      tension: 0.1
+
+      // The actual data
+      data: [65, 59, 80, 81, 56, 55, 40],
+
+      // String - If specified, binds the dataset to a certain y-axis. If not specified, the first y-axis is used.
+      yAxisID: "y-axis-1",
+    },
+    {
+      label: "My Second dataset",
+      fill: false,
+      backgroundColor: "rgba(220,220,220,0.2)",
+      borderColor: "rgba(220,220,220,1)",
+      pointBorderColor: "rgba(220,220,220,1)",
+      pointBackgroundColor: "#fff",
+      pointBorderWidth: 1,
+      pointHoverRadius: 5,
+      pointHoverBackgroundColor: "rgba(220,220,220,1)",
+      pointHoverBorderColor: "rgba(220,220,220,1)",
+      pointHoverBorderWidth: 2,
+      data: [28, 48, 40, 19, 86, 27, 90]
     }
-    var chartOptions = {
-      scaleGridLineColor : "rgba(0,0,0,.03)"
-    }
-//Get context with jQuery - using jQuery's .get() method.
-var runTotal = $("#runTotal").get(0).getContext("2d");
-//This will get the first returned node in the jQuery collection.
-var myLineChart = new Chart(runTotal).Line(weeklyRunTotals, chartOptions);
+  ]
+};
+
+var myLineChart = new Chart(ctx, {
+  type: 'line',
+  data: data,
+  options: options
+});
