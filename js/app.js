ce9802dfa94eba58b3e487fa39a007939ab6c470
Sam Hankins <samantha.hankins@icloud.com>
2015-12-27 12:29:52 -0600

    blocfit with old version chartjs

--- js/app.js
+++ js/app.js
@@ -24,7 +24,54 @@ var weeklyRunTotals = {
     var chartOptions = {
       scaleGridLineColor : "rgba(0,0,0,.03)"
     }
+
 //Get context with jQuery - using jQuery's .get() method.
 var runTotal = $("#runTotal").get(0).getContext("2d");
 //This will get the first returned node in the jQuery collection.
 var myLineChart = new Chart(runTotal).Line(weeklyRunTotals, chartOptions);
+
+// Doughnut Chart for Weekly Goals
+var weeklyGoal = [
+  {
+    value: 10,
+    color:"#4eddc1"
+  },
+  {
+    value : 90,
+    color : "#E2EAE9"
+  }
+]
+var options = {
+  percentageInnerCutout : 80
+};
+var thisWeek = $("#weeklyGoal").get(0).getContext("2d");
+var myWeeklyGoal = new Chart(thisWeek).Doughnut(weeklyGoal, options);
+
+
+// Monthly Goals
+var monthlyGoal = [
+  {
+    value: 95,
+    color:"#1abc9c"
+  },
+  {
+    value : 5,
+    color : "#E2EAE9"
+  }
+]
+var thisMonth = $("#monthlyGoal").get(0).getContext("2d");
+var myMonthlyGoal = new Chart(thisMonth).Doughnut(monthlyGoal, options);
+
+// Annual Goals
+var annualGoal = [
+  {
+    value: 72,
+    color:"#0b9277",
+  },
+  {
+    value : 28,
+    color : "#E2EAE9"
+  }
+]
+var thisYear = $("#annualGoal").get(0).getContext("2d");
+var myAnnualGoal = new Chart(thisYear).Doughnut(annualGoal, options);
